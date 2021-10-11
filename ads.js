2ede8d5ba37d439c8e8555ef12c2403cc2b6ee2c
BIG-MACHINE-OF-\pulki <pulkitname@gmail.com>
2021-10-11 14:24:36 +0530

    no message

--- ads.js
+++ ads.js
@@ -65,7 +65,8 @@ function playAds() {
 
   try {
     // Initialize the ads manager. Ad rules playlist will start at this time.
-    adsManager.init(768, 432, google.ima.ViewMode.NORMAL);
+    //adsManager.init(768, 432, google.ima.ViewMode.NORMAL);
+	adsManager.init(window.innerWidth, window.innerHeight, google.ima.ViewMode.NORMAL);
     // Call play to start showing the ad. Single video and overlay ads will
     // start at this time; the call will be ignored for ad rules.
     adsManager.start();
